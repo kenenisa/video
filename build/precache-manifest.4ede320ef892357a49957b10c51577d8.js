ee18bcde01d25a3bd0718392edd14233cf85032c
Kenenisa Alemayhu <kenenisaalemayhu0@gmail.com>
2020-02-01 16:10:17 +0300

    deployment to github upload with build

---
+++ build/precache-manifest.4ede320ef892357a49957b10c51577d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25cb0c462494bf651932c8cb7050835a",
+    "url": "/video/index.html"
+  },
+  {
+    "revision": "b8a8653a0065a5bdc316",
+    "url": "/video/static/css/main.2d845bce.chunk.css"
+  },
+  {
+    "revision": "c5f1adb0f5118a13e51d",
+    "url": "/video/static/js/2.0557b1bc.chunk.js"
+  },
+  {
+    "revision": "b8a8653a0065a5bdc316",
+    "url": "/video/static/js/main.eb1d6237.chunk.js"
+  },
+  {
+    "revision": "4a8e02415251fdbeb6be",
+    "url": "/video/static/js/runtime-main.0398e746.js"
+  }
+]);
